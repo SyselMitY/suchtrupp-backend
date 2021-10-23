3cc0a650a44a26373f51d5ca8502e23c84ff5c0c
Peter Sysel <peter@sysel.at>
2021-10-23 13:26:37 +0200

    project initialized

---
+++ src/main/java/cf/soisi/suchtrupp_erfassung/SuchtruppErfassungApplication.java
@@ -0,0 +1,13 @@
+package cf.soisi.suchtrupp_erfassung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuchtruppErfassungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuchtruppErfassungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cf/soisi/suchtrupp_erfassung/SuchtruppErfassungApplicationTests.java
@@ -0,0 +1,13 @@
+package cf.soisi.suchtrupp_erfassung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuchtruppErfassungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
